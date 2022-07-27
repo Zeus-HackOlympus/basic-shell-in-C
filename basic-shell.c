5d6b154cc2b7c0ac935771ecbcc374176a7d3f25
Zeus-HackOlympus <vishaljuneja707@gmail.com>
2022-07-27 21:08:22 +0530

    first commit

---
+++ basic-shell.c
@@ -0,0 +1,112 @@
+/**
+ * @author      : HackOlympus (st4rk@ubuntu)
+ * @file        : basic-shell
+ * @created     : Tuesday Jul 26, 2022 22:40:12 MST
+ */
+
+#include <stdio.h> 
+#include <string.h> 
+#include <signal.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <limits.h> 
+
+#define MAX_ARGC 10
+
+int argc ; 
+char *argv[MAX_ARGC] = {NULL};  
+int isBackground = 0; 
+
+void print_prompt()
+{
+    char cwd[PATH_MAX+5];
+    getcwd(cwd, PATH_MAX); 
+    strcat(cwd,"% ");
+    printf("%s",cwd); 
+    fflush(NULL); 
+}
+
+int take_input()
+{
+    /* take input and split it into tokens */
+    /* returns the number of args passed */ 
+    
+    char line[PATH_MAX]; // THIS IS A BUG //
+    
+    if (!fgets(line,sizeof line,stdin)) 
+        return 0; 
+    line[strcspn(line, "\n")] = 0; // remove trailing new line
+    fflush(stdin); 
+    /* split input into tokens for argv */ 
+    char *token; 
+    token = strtok(line," "); 
+    int args=0; 
+    while (token != NULL && args < MAX_ARGC)   
+    {
+        if (strcmp(token, "\t") && strcmp(token,"\n")) {
+            argv[args++] = token;  
+            token = strtok(NULL, " "); 
+        }
+    }
+
+    return args;     
+}
+
+void print_args()
+{
+    printf("argc: %d\n",argc); 
+    printf("Arguments: "); 
+    for (int i=0; i < MAX_ARGC && argv[i] != NULL; i++) printf("%s ", argv[i]);  
+    puts(""); 
+}
+
+int pwd()
+{
+    /* print working directory */
+    /* path max is defined in limits.h */
+    char cwd[PATH_MAX];
+    if ((getcwd(cwd, PATH_MAX)) != NULL) {
+        printf("%s\n",cwd);
+    }
+    else {
+        return -1; 
+    }
+    return 0; 
+}
+
+int whoami()
+{
+    int uid = getuid(); 
+    char *username = getlogin(); 
+    printf("%d(%s)\n", uid,username); 
+    return 0; 
+}
+
+int main()
+{
+    // signal(SIGINT, SIG_IGN);
+    while(1) {
+        print_prompt(); 
+        argc = take_input();
+        if (argc == 0) {
+            puts("Exiting..."); 
+            exit(0); 
+        }
+        
+        // if command has to be run in background
+        if (!strcmp(argv[argc-1],"&")) 
+            isBackground = 1; 
+
+        //print_args();    
+        print_args(); 
+        if (!strcmp(argv[0], "pwd")) {
+            pwd();
+        } else if (!strcmp(argv[0], "whoami")) {
+            whoami();
+        } else {
+            printf("%s Not implemented\n",argv[0]); 
+        }
+        fflush(NULL); 
+    }
+    return 0; 
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+A basic shell in C (made this just to practice my C coding skills and cuz I was bored xD) 
+
+Inspiration : 
+
+- pwncollege discord server 
+- https://archive.org/details/github.com-rby90-project-based-tutorials-in-c_-_2020-10-01_09-52-22 
+- https://github.com/tokenrove/build-your-own-shell
+
+References : 
+- https://github.com/tokenrove/build-your-own-shell
+- https://www.cs.purdue.edu/homes/grr/SystemsProgrammingBook/Book/Chapter5-WritingYourOwnShell.pdf
+- https://www.cs.cornell.edu/courses/cs414/2004su/homework/shell/shell.html
+- https://www.cs.cmu.edu/afs/cs/academic/class/15213-s02/www/applications/labs/lab5/shlab.html
+
+Todo: 
+- Add more command implementation or simple execve the existing system binaries (that exist in PATH)
+- Understand how to make parser using lex and yacc and implementing it in this project 
